872fcbcbd02bd91fb751b5ee2b0fcc88935eb3a7
Hoareau <contact@jacky-hoareau.com>
2013-06-09 12:42:39 +0200

    modif live module

---
+++ pyTeleinfo/Modules/LiveModule.py
@@ -0,0 +1,28 @@
+'''
+Created on 9 juin 2013
+
+@author: Hoareau
+'''
+from Connectors import SerialConnector
+from Util.Network import Network
+import tornado.web
+
+class LiveModule(tornado.web.UIModule):
+
+    def render(self):
+        return ''
+
+    
+    def embedded_javascript(self):
+        
+        self.ser=SerialConnector()
+        self.ser.getDatas()
+        
+        n=Network()
+        
+        if n.isWAN(self.request.remote_ip):
+            namespace="smartdomus.redirectme.net:9090"
+        else:
+            namespace="http://"+n.getPrivateIp()+":"+str(9090)
+     
+        return "power="+str(self.ser.getPower())+"; var namespace=\""+namespace+"\";"
